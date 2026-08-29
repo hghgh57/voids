abe596df51fc843ff6ba7eaa26e3ea30f933dcb3
hghgh57 <rizzbut3@gmail.com>
2026-08-29 18:02:26 +1000

    Update permissions.js

--- galy-bot-fixed/galy-bot-fixed/gaming-bot/utils/permissions.js
+++ galy-bot-fixed/galy-bot-fixed/gaming-bot/utils/permissions.js
@@ -2,12 +2,27 @@ const config = require('../config.json');
 
 function isAdmin(member) {
   const roleIds = config.adminRoleIds || [];
-  return roleIds.some((id) => id && !id.startsWith('PUT_') && member.roles.cache.has(id));
+
+  return roleIds.some(
+    (id) =>
+      id &&
+      !id.startsWith('PUT_') &&
+      member.roles.cache.has(id)
+  );
 }
 
 function isMod(member) {
   const roleIds = config.modRoleIds || [];
-  return roleIds.some((id) => id && !id.startsWith('PUT_') && member.roles.cache.has(id));
+
+  return roleIds.some(
+    (id) =>
+      id &&
+      !id.startsWith('PUT_') &&
+      member.roles.cache.has(id)
+  );
 }
 
-module.exports = { isAdmin, isMod };
+module.exports = {
+  isAdmin,
+  isMod,
+};
