353f867af46f8a3d69a8d2271d4ec34b55806c59
hghgh57 <rizzbut3@gmail.com>
2026-09-05 21:33:11 +1000

    Update musicsetup.js

--- galy-bot-fixed/galy-bot-fixed/gaming-bot/commands/musicsetup.js
+++ galy-bot-fixed/galy-bot-fixed/gaming-bot/commands/musicsetup.js
@@ -68,16 +68,6 @@ module.exports = {
       });
     }
 
-    const oldMessages = await controlChannel.messages.fetch({
-      limit: 100,
-    });
-
-    for (const message of oldMessages.values()) {
-      if (message.author.id === interaction.client.user.id) {
-        await message.delete().catch(() => {});
-      }
-    }
-
     const embed = new EmbedBuilder()
       .setColor(0x5865F2)
       .setTitle("🎵 Void's Music")
@@ -158,16 +148,9 @@ module.exports = {
       panelMessageId: panel.id,
     });
 
-    const state = createGuildPlayer(guild.id);
+    createGuildPlayer(guild.id);
 
-    state.panelChannelId = controlChannel.id;
-    state.panelMessageId = panel.id;
-
-    await setPanel(
-      guild.id,
-      controlChannel.id,
-      panel.id
-    );
+    await setPanel(guild.id, controlChannel.id, panel.id);
 
     await interaction.editReply(
       `✅ Music system created!\n\n` +
